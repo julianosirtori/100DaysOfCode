956a1c75402a98558087ef4c9c1467535562fe58
Juliano Sirtori <julianosirtori@gmail.com>
2020-04-20 07:14:48 -0300

    Init form

--- 002-dog-calculator/src/pages/Home/index.js
+++ 002-dog-calculator/src/pages/Home/index.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-import { Container, Title } from './styles';
+import { Container, Title, Form, Button, ButtonText, PickerSize, SubmitButton, SubmitButtonText } from './styles';
 
 export default function Home() {
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [birth, setBirth] = useState(new Date());
+
   return (
     <Container>
-      <Title>Calculator Dog</Title>
+      <Title>Quantos anos tem seu cachorro</Title>
+
+      <Form>
+          <Button onPress={() => setShowDatePicker(!showDatePicker)}>
+            <ButtonText>Quando seu dog nasceu</ButtonText>
+          </Button>
+          <PickerSize>
+            <PickerSize.Item label="small" value="small" />
+            <PickerSize.Item label="medium" value="medium" />
+            <PickerSize.Item label="big" value="big" />
+          </PickerSize>
+          {showDatePicker && (
+            <DateTimePicker
+              is24Hour={true}
+              value={birth}
+              display="default"
+            />
+          )}
+          <SubmitButton>
+            <SubmitButtonText>Calcular</SubmitButtonText>
+          </SubmitButton>
+
+      </Form>
+      
     </Container>
   );
 }
--- 002-dog-calculator/src/pages/Home/styles.js
+++ 002-dog-calculator/src/pages/Home/styles.js
@@ -5,3 +5,17 @@ export const Container = styled.View`
 `;
 
 export const Title = styled.Text``;
+
+export const Form = styled.View` `;
+
+export const Button = styled.TouchableOpacity`
+  margin-top: 32px;
+`;
+
+export const ButtonText = styled.Text``;
+
+export const PickerSize = styled.Picker``;
+
+export const SubmitButton  = styled.TouchableOpacity``;
+
+export const SubmitButtonText = styled.Text``;
