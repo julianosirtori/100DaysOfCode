2529234aebc109a385c5ac3a6222f69cb590fe9b
Juliano Sirtori <julianosirtori@gmail.com>
2020-04-20 06:55:54 -0300

    Init project 002

---
+++ 002-dog-calculator/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+import Home from './src/pages/Home';
+
+export default function App() {
+  return (
+    <Home />
+  );
+}
