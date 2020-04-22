aa7f53b30fe77639ce11dd50c783cf45cd10faaa
Juliano Sirtori <julianosirtori@gmail.com>
2020-04-22 20:02:52 -0300

    Initialized project real to dolar

---
+++ 004-real-to-dolar/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>HomePage</h1>
+    </Container>
+  );
+}
