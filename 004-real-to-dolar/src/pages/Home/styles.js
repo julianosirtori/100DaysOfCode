c1d8e908efa78afc5189aabf2b791ecc7324c8c1
Juliano Sirtori <julianosirtori@gmail.com>
2020-04-22 22:39:00 -0300

    Finish

--- 004-real-to-dolar/src/pages/Home/styles.js
+++ 004-real-to-dolar/src/pages/Home/styles.js
@@ -1,5 +1,75 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  background: #4292bf;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  h1{
+    margin-bottom: 32px;
+    color: #FFF;
+  }
+`;
+
+export const Center = styled.div`
+  width: 100%;
+  max-width: 360px;
+  border-radius: 4;
+  border-radius: 4px;
+  box-shadow: 0 0  10px #2229;
+  background: #ffff;
   
+  form{
+    display: flex;
+    flex-direction: column;
+    font-size: 16px;
+    padding: 16px 32px 32px 32px;
+
+    input{
+      width: 100%;
+      margin-bottom: 8px;
+      text-align: center;
+      padding: 4px 8px;
+      font-size: 16px;
+      border-radius: 4px;
+      border: 1px solid #dedede;
+    }
+
+    button{
+      margin-top: 16px;
+      color: #fff;
+      border: none;
+      font-size: 16px;
+      border-radius: 4px;
+      font-weight: bold;
+      padding: 8px;
+      background: #33875d;
+      
+      &:hover{
+        background: #1c4f36;
+      }
+
+      &:active{
+        background: #88d8b0;
+      }
+
+    }
+  }
+`;
+
+export const Resultado = styled.div`
+  display:flex;
+  flex-direction: column;
+  font-size: 18px;
+  justify-content: center;
+  align-items: center;
+  color: #1c4f36;
+
+  span{
+    margin-bottom: 8px;
+  }
 `;
--- 004-real-to-dolar/src/services/api.js
+++ 004-real-to-dolar/src/services/api.js
@@ -1,5 +1,20 @@
 import axios from 'axios';
+import { format } from 'date-fns';
 
-const api = axios.create();
+const api = axios.create(
+  {
+    baseURL: 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata',
+  },
+);
 
 export default api;
+
+export function getQuotation(data) {
+  const dataFormated = format(data, 'MM-dd-yyyy');
+  return api.get(`/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${dataFormated}'&$top=100&$format=json`);
+}
+
+export function getQuotationNow() {
+  const dataFormated = format(new Date(), 'MM-dd-yyyy');
+  return api.get(`/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='${dataFormated}'&$top=100&$format=json`);
+}
