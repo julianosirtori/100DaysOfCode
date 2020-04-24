a1a27447480a2383f7cb8093d500f79ef5f54f54
Juliano Sirtori <julianosirtori@gmail.com>
2020-04-23 23:34:19 -0300

    Add Timer

--- 005-pomodoro-app/src/pages/Home/index.js
+++ 005-pomodoro-app/src/pages/Home/index.js
@@ -1,15 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { set, format } from 'date-fns';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  set, format, getTime, subSeconds,
+} from 'date-fns';
 
 import {
   Container, ContainerButtons, Button, ButtonText, ClockText,
 } from './styles';
 
 export default function Home() {
-  const [time, setTime] = useState('');
+  const [time, setTime] = useState(0);
+  const [statusTime, setStatusTime] = useState('stop');
+
+  const timeRef = useRef(time);
+  const intervalRef = useRef();
 
   function setTimeMinutes(minutes) {
-    setTime(format(set(new Date(), { minutes, seconds: 0 }), 'mm:ss'));
+    const newTime = getTime(set(new Date(), { minutes, seconds: 0 }));
+    setTime(newTime);
+    timeRef.current = newTime;
   }
 
   useEffect(() => {
@@ -17,16 +25,76 @@ export default function Home() {
   }, []);
 
 
+  function startTime() {
+    setStatusTime('started');
+    intervalRef.current = setInterval(() => {
+      timeRef.current = getTime(subSeconds(timeRef.current, 1));
+      setTime(timeRef.current);
+    }, 1000);
+  }
+
+  function pauseTime() {
+    setStatusTime('paused');
+    clearInterval(intervalRef.current);
+  }
+
+  function stopTime() {
+    setStatusTime('stop');
+    clearInterval(intervalRef.current);
+    setTimeMinutes(25);
+  }
+
+  function saveTime() {
+
+  }
+
+  function getButton() {
+    switch (statusTime) {
+      case 'stop':
+        return (
+          <>
+            <Button onPress={startTime}>
+              <ButtonText>INCIAR</ButtonText>
+            </Button>
+            <Button onPress={stopTime}>
+              <ButtonText>PARAR</ButtonText>
+            </Button>
+          </>
+        );
+      case 'started':
+        return (
+          <>
+            <Button onPress={pauseTime}>
+              <ButtonText>PAUSAR</ButtonText>
+            </Button>
+            <Button onPress={stopTime}>
+              <ButtonText>PARAR</ButtonText>
+            </Button>
+          </>
+        );
+      case 'paused':
+        return (
+          <>
+            <Button onPress={startTime}>
+              <ButtonText>CONTINUAR</ButtonText>
+            </Button>
+            <Button onPress={saveTime}>
+              <ButtonText>FEITO</ButtonText>
+            </Button>
+          </>
+        );
+      default:
+        return '';
+    }
+  }
+
   return (
     <Container>
-      <ClockText>{time}</ClockText>
+      <ClockText>{format(time, 'mm:ss')}</ClockText>
       <ContainerButtons>
-        <Button>
-          <ButtonText>INICIAR</ButtonText>
-        </Button>
-        <Button>
-          <ButtonText>PARAR</ButtonText>
-        </Button>
+        {
+         getButton()
+        }
       </ContainerButtons>
     </Container>
   );
